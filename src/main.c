70a76f66a6f1b4159c3c0b41f6f74024a6656934
liveusr <liveusr@live.com>
2016-03-20 01:56:30 -0400

    Cleaned-up default code

--- src/main.c
+++ src/main.c
@@ -13,6 +13,9 @@ static GBitmap *s_background_bitmap;
 static GFont s_time_font;
 static GFont s_weather_font;
 
+/******************************************************************************/
+/******************************************************************************/
+
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // Store incoming information
   static char temperature_buffer[8];
@@ -46,6 +49,9 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
+/******************************************************************************/
+/******************************************************************************/
+
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
@@ -59,6 +65,9 @@ static void update_time() {
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
 }
+
+/******************************************************************************/
+/******************************************************************************/
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
@@ -77,11 +86,16 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   }
 }
 
+/******************************************************************************/
+/******************************************************************************/
+
 static void main_window_load(Window *window) {
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  
+  
   // Create GBitmap
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
 
@@ -89,9 +103,11 @@ static void main_window_load(Window *window) {
   s_background_layer = bitmap_layer_create(bounds);
 
   // Set the bitmap onto the layer and add to the window
-  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+  //bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  //layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
 
+  
+  
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
@@ -106,25 +122,28 @@ static void main_window_load(Window *window) {
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
 
   // Apply to TextLayer
-  text_layer_set_font(s_time_layer, s_time_font);
+  //text_layer_set_font(s_time_layer, s_time_font);
 
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  //layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
+  
+  
   // Create temperature Layer
   s_weather_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(125, 120), bounds.size.w, 25));
 
   // Style the text
   text_layer_set_background_color(s_weather_layer, GColorClear);
-  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_color(s_weather_layer, GColorBlack);
   text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
   text_layer_set_text(s_weather_layer, "Loading...");
 
   // Create second custom font, apply it and add to Window
-  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
-  text_layer_set_font(s_weather_layer, s_weather_font);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  //s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_COMIC_SANS_16));
+  //text_layer_set_font(s_weather_layer, s_weather_font);
+  //layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
 }
 
 static void main_window_unload(Window *window) {
@@ -146,12 +165,16 @@ static void main_window_unload(Window *window) {
 }
 
 
+/******************************************************************************/
+/******************************************************************************/
+
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
 
   // Set the background color
-  window_set_background_color(s_main_window, GColorBlack);
+  //window_set_background_color(s_main_window, GColorBlack);
+  window_set_background_color(s_main_window, GColorClear);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -163,10 +186,10 @@ static void init() {
   window_stack_push(s_main_window, true);
 
   // Make sure the time is displayed from the start
-  update_time();
+  //update_time();
 
   // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  //tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
